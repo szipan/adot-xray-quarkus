6973387248af73663c9529f814987fb8489b5d87
szipan <szipan@outlook.com>
2023-05-03 14:57:36 +0000

    Quarkus Initialization

---
+++ src/test/java/cn/nwcdcloud/dynamodb/FruitResourceIT.java
@@ -0,0 +1,8 @@
+package cn.nwcdcloud.dynamodb;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FruitResourceIT extends FruitResourceTest {
+    // Execute the same tests but in packaged mode.
+}
